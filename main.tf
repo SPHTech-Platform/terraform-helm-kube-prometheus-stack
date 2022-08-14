5592604cd87a8cc94eabeb5b8fae5f045ad11736
franklinpashok <franklinpashok@gmail.com>
2022-08-14 11:31:28 +0800

    Updated Grafana Chart Values

--- main.tf
+++ main.tf
@@ -4,28 +4,113 @@ locals {
     prometheus_image_tag        = var.prometheus_image_tag
     prometheus_host_url         = var.prometheus_host_url
 
-    grafana_image_repository = var.grafana_image_repository
-    grafana_image_tag        = var.grafana_image_tag
-    grafana_admin_password   = var.grafana_admin_password
-    grafana_host_url         = var.grafana_host_url
-    grafana_server_root_url  = var.grafana_server_root_url
-
-    grafana_okta_client_id     = var.grafana_okta_client_id
-    grafana_okta_client_secret = var.grafana_okta_client_secret
-    grafana_okta_auth_url      = var.grafana_okta_auth_url
-    grafana_okta_token_url     = var.grafana_okta_token_url
-    grafana_okta_api_url       = var.grafana_okta_api_url
-
-    grafana_s3_image_bucket        = var.grafana_s3_image_bucket
-    grafana_s3_image_bucket_region = var.grafana_s3_image_bucket_region
-
     prometheus_operator_image_repository = var.prometheus_operator_image_repository
     prometheus_operator_image_tag        = var.prometheus_operator_image_tag
 
-    smtp_host         = var.smtp_host
-    smtp_user         = var.smtp_user
-    smtp_password     = var.smtp_password
-    smtp_from_address = var.smtp_from_address
+    ################################
+    ######## GRAFANA LOCALS ########
+    ################################
+    replicas                  = var.replicas
+    image                     = var.image
+    tag                       = var.tag
+    grafana_image_pull_policy = var.grafana_image_pull_policy
+
+    grafana_service_account             = var.grafana_service_account
+    grafana_service_account_annotations = jsonencode(var.grafana_service_account_annotations)
+
+    grafana_psp_enable        = var.grafana_psp_enable
+    grafana_psp_use_app_armor = var.grafana_psp_use_app_armor
+    grafana_pdb               = jsonencode(var.grafana_pdb)
+    grafana_command           = jsonencode(var.grafana_command)
+
+    grafana_extra_configmap_mounts = jsonencode(var.grafana_extra_configmap_mounts)
+    grafana_extra_empty_dir_mounts = jsonencode(var.grafana_extra_empty_dir_mounts)
+
+    grafana_priority_class_name = var.grafana_priority_class_name
+
+    grafana_pod_annotations  = jsonencode(var.grafana_pod_annotations)
+    grafana_annotations      = jsonencode(var.grafana_annotations)
+    grafana_resources        = jsonencode(var.grafana_resources)
+    grafana_node_selector    = jsonencode(var.grafana_node_selector)
+    grafana_tolerations      = jsonencode(var.grafana_tolerations)
+    grafana_affinity         = jsonencode(var.grafana_affinity)
+    grafana_security_context = jsonencode(var.grafana_security_context)
+
+    grafana_env                 = jsonencode(var.grafana_env)
+    grafana_env_from_secret     = var.grafana_env_from_secret
+    grafana_extra_secret_mounts = jsonencode(var.grafana_extra_secret_mounts)
+    grafana_extra_volume_mounts = jsonencode(var.grafana_extra_volume_mounts)
+
+    grafana_enable_service_links = var.grafana_enable_service_links
+
+    grafana_service_type        = var.grafana_service_type
+    grafana_service_port        = var.grafana_service_port
+    grafana_service_target_port = var.grafana_service_target_port
+    grafana_service_annotations = jsonencode(var.grafana_service_annotations)
+    grafana_service_labels      = jsonencode(var.grafana_service_labels)
+
+    grafana_ingress_enabled     = var.grafana_ingress_enabled
+    grafana_ingress_annotations = jsonencode(var.grafana_ingress_annotations)
+    grafana_ingress_labels      = jsonencode(var.grafana_ingress_labels)
+    grafana_ingress_hosts       = jsonencode(var.grafana_ingress_hosts)
+    grafana_ingress_tls         = jsonencode(var.grafana_ingress_tls)
+
+    grafana_extra_init_containers = jsonencode(var.grafana_extra_init_containers)
+    grafana_extra_containers      = jsonencode(var.grafana_extra_containers)
+
+    grafana_persistence_enabled            = var.grafana_persistence_enabled
+    grafana_persistence_size               = var.grafana_persistence_size
+    grafana_persistence_storage_class_name = var.grafana_persistence_storage_class_name
+    grafana_persistence_annotations        = jsonencode(var.grafana_persistence_annotations)
+    grafana_persistence_existing_claim     = var.grafana_persistence_existing_claim
+
+    grafana_init_chown_data_enabled   = var.grafana_init_chown_data_enabled
+    grafana_init_chown_data_resources = jsonencode(var.grafana_init_chown_data_resources)
+
+    grafana_plugins = jsonencode(var.grafana_plugins)
+
+    grafana_datasources            = indent(2, var.grafana_datasources)
+    grafana_notifiers              = indent(2, var.grafana_notifiers)
+    grafana_dashboard_providers    = indent(2, var.grafana_dashboard_providers)
+    grafana_dashboards             = indent(2, var.grafana_dashboards)
+    grafana_dashboards_config_maps = indent(2, var.grafana_dashboards_config_maps)
+
+    grafana_main_config         = indent(2, var.grafana_main_config)
+    grafana_okta_config         = indent(2, var.grafana_okta_config)
+    grafana_db_config           = indent(2, var.grafana_db_config)
+    grafana_okta_enabled        = var.grafana_okta_enabled
+    GRAFANA_OAUTH_CLIENT_ID     = var.GRAFANA_OAUTH_CLIENT_ID
+    GRAFANA_OAUTH_CLIENT_SECRET = var.GRAFANA_OAUTH_CLIENT_SECRET
+
+    grafana_image_repository = var.grafana_image_repository
+    grafana_image_tag        = var.grafana_image_tag
+
+    cluster_name            = var.cluster_name
+    cluster_oidc_issuer_url = var.cluster_oidc_issuer_url
+    cluster_oidc_issuer_arn = var.cluster_oidc_issuer_arn
+
+    grafana_ldap_existing_secret = var.grafana_ldap_existing_secret
+    grafana_ldap_config          = jsonencode(var.grafana_ldap_config)
+
+    grafana_smtp_existing_secret = var.grafana_smtp_existing_secret
+    grafana_smtp_user_key        = var.grafana_smtp_user_key
+    grafana_smtp_password_key    = var.grafana_smtp_password_key
+
+    grafana_admin_password_key = var.grafana_admin_password_key
+
+    image_renderer_enabled             = var.image_renderer_enabled
+    image_renderer_replicas            = var.image_renderer_replicas
+    image_renderer_image_repository    = var.image_renderer_image_repository
+    image_renderer_image_tag           = var.image_renderer_image_tag
+    image_renderer_service_account     = var.image_renderer_service_account
+    image_renderer_port                = var.image_renderer_port
+    image_renderer_target_port         = var.image_renderer_target_port
+    image_renderer_priority_class_name = var.image_renderer_priority_class_name
+    image_renderer_env                 = jsonencode(var.image_renderer_env)
+    image_renderer_annotations         = jsonencode(var.image_renderer_annotations)
+    image_renderer_pod_annotations     = jsonencode(var.image_renderer_pod_annotations)
+    image_renderer_security_context    = jsonencode(var.image_renderer_security_context)
+    image_renderer_resources           = jsonencode(var.image_renderer_resources)
   }
 }
 
@@ -41,5 +126,6 @@ resource "helm_release" "kube_prometheus_stack" {
 
   values = [
     templatefile("${path.module}/templates/values.yaml", local.values),
+    #    templatefile("${path.module}/templates/okta_config.yaml", local.values)
   ]
 }
--- variables.tf
+++ variables.tf
@@ -74,10 +74,10 @@ variable "grafana_image_tag" {
   default     = "9.0.2"
 }
 
-variable "grafana_admin_password" {
-  description = "Grafana Admin Password"
+variable "grafana_admin_password_key" {
+  description = "Key in the secret containing the admin password"
   type        = string
-  sensitive   = true
+  default     = "admin-password"
 }
 
 variable "grafana_host_url" {
@@ -88,37 +88,6 @@ variable "grafana_host_url" {
 
 variable "grafana_server_root_url" {
   description = "Grafana Server Root URL"
-  type        = string
-  default     = ""
-}
-
-### OKAT Config of Grafana ###
-variable "grafana_okta_client_id" {
-  description = "OKTA Client ID for Grafana"
-  type        = string
-  sensitive   = true
-}
-
-variable "grafana_okta_client_secret" {
-  description = "OKTA Client Secret for Grafana"
-  type        = string
-  sensitive   = true
-}
-
-variable "grafana_okta_auth_url" {
-  description = "OKTA Auth URL for Grafana"
-  type        = string
-  default     = ""
-}
-
-variable "grafana_okta_token_url" {
-  description = "OKTA Token URL for Grafana"
-  type        = string
-  default     = ""
-}
-
-variable "grafana_okta_api_url" {
-  description = "OKTA API URL for Grafana"
   type        = string
   default     = ""
 }
@@ -135,31 +104,6 @@ variable "prometheus_operator_image_tag" {
   default     = "v0.57.0"
 }
 
-### SMTP ####
-variable "smtp_host" {
-  description = "SMTP Server Host Address"
-  type        = string
-  default     = ""
-}
-
-variable "smtp_user" {
-  description = "SMTP User"
-  type        = string
-  sensitive   = true
-}
-
-variable "smtp_password" {
-  description = "SMTP Password"
-  type        = string
-  sensitive   = true
-}
-
-variable "smtp_from_address" {
-  description = "From Address which want to send emails"
-  type        = string
-  default     = ""
-}
-
 ### S3 ###
 variable "grafana_s3_image_bucket" {
   description = "Grafana S3 image bucket"
@@ -171,4 +115,505 @@ variable "grafana_s3_image_bucket_region" {
   description = "Region of the Grafana S3 image bucket"
   type        = string
   default     = ""
+}
+
+###########################################
+######## GRAFANA LOCALS & VARIABLE ########
+###########################################
+variable "recreate_pods" {
+  description = "Recreate the pods with every helm update"
+  type        = bool
+  default     = "false"
+}
+
+
+variable "grafana_service_account" {
+  description = "Name of the Service Account for Grafana"
+  type        = string
+  default     = ""
+}
+
+variable "grafana_service_account_annotations" {
+  description = "Annotations for service account"
+  type        = map(any)
+  default     = {}
+}
+
+variable "replicas" {
+  description = "Number of replicas of Grafana to run"
+  type        = number
+  default     = 1
+}
+
+variable "image" {
+  description = "Docker Image for Grafana"
+  type        = string
+  default     = "grafana/grafana"
+}
+
+variable "tag" {
+  description = "Docker Image tag for Grafana"
+  type        = string
+  default     = "9.0.3"
+}
+
+variable "grafana_image_pull_policy" {
+  description = "Image Pull Policy for Grafana"
+  type        = string
+  default     = "IfNotPresent"
+}
+
+variable "grafana_extra_configmap_mounts" {
+  description = "Extra ConfigMap to mount into the Container"
+  type        = list(any)
+  default     = []
+}
+
+variable "grafana_extra_empty_dir_mounts" {
+  description = "Extra Empty DIRs to mount into the Container"
+  type        = list(any)
+  default     = []
+}
+
+variable "grafana_priority_class_name" {
+  description = "Priority Class name for Grafana"
+  type        = string
+  default     = ""
+}
+
+variable "grafana_pod_annotations" {
+  description = "Pod annotations"
+  type        = map(any)
+  default     = {}
+}
+
+variable "grafana_annotations" {
+  description = "Deployment annotations"
+  type        = map(any)
+  default     = {}
+}
+
+variable "grafana_service_type" {
+  description = "Service type"
+  type        = string
+  default     = "ClusterIP"
+}
+
+variable "grafana_service_port" {
+  description = "Port of the service"
+  type        = string
+  default     = "80"
+}
+
+variable "grafana_service_target_port" {
+  description = "Port in container to expose service"
+  type        = string
+  default     = "3000"
+}
+
+variable "grafana_service_annotations" {
+  description = "Annotations for the service"
+  type        = map(any)
+  default     = {}
+}
+
+variable "grafana_service_labels" {
+  description = "Labels for the service"
+  type        = map(any)
+  default     = {}
+}
+
+### Grafana Ingress ###
+variable "grafana_ingress_enabled" {
+  description = "Enable Ingress"
+  type        = bool
+  default     = false
+}
+
+variable "grafana_ingress_annotations" {
+  description = "Annotations for ingress"
+  type        = map(any)
+  default     = {}
+}
+
+variable "grafana_ingress_labels" {
+  description = "Labels for ingress"
+  type        = map(any)
+  default     = {}
+}
+
+variable "grafana_ingress_hosts" {
+  description = "Hosts for ingress"
+  type        = list(any)
+  default     = []
+}
+
+variable "grafana_ingress_tls" {
+  description = "TLS configuration for ingress"
+  type        = list(any)
+  default     = []
+}
+
+variable "grafana_resources" {
+  description = "Resources for Grafana container"
+  type        = map(any)
+  default     = {}
+}
+
+variable "grafana_node_selector" {
+  description = "Node selector for Pods"
+  type        = map(any)
+  default     = {}
+}
+
+variable "grafana_tolerations" {
+  description = "Tolerations for pods"
+  type        = list(any)
+  default     = []
+}
+
+variable "grafana_affinity" {
+  description = "Pod affinity"
+  type        = map(any)
+  default     = {}
+}
+
+variable "grafana_security_context" {
+  description = "Security context for pods defined as a map which will be serialized to JSON."
+  type        = any
+  default = {
+    runAsGroup = 472
+    runAsUser  = 472
+    fsGroup    = 472
+  }
+}
+
+variable "grafana_extra_init_containers" {
+  description = "Extra init containers"
+  type        = list(any)
+  default     = []
+}
+
+variable "grafana_extra_containers" {
+  description = "YAML string for extra containers"
+  type        = string
+  default     = ""
+}
+
+variable "grafana_persistence_enabled" {
+  description = "Enable PV"
+  type        = bool
+  default     = false
+}
+
+variable "grafana_persistence_storage_class_name" {
+  description = "Storage Class name for the PV"
+  type        = string
+  default     = "default"
+}
+
+variable "grafana_persistence_annotations" {
+  description = "Annotations for the PV"
+  type        = map(any)
+  default     = {}
+}
+
+variable "grafana_persistence_size" {
+  description = "Size of the PV"
+  type        = string
+  default     = "10Gi"
+}
+
+variable "grafana_persistence_existing_claim" {
+  description = "Use an existing PVC"
+  type        = string
+  default     = ""
+}
+
+variable "grafana_init_chown_data_enabled" {
+  description = "Enable the Chown init container"
+  type        = bool
+  default     = true
+}
+
+variable "grafana_init_chown_data_resources" {
+  description = "Resources for the Chown init container"
+  type        = map(any)
+  default     = {}
+}
+
+variable "grafana_env" {
+  description = "Extra environment variables that will be pass onto deployment pods"
+  type        = map(any)
+  default     = {}
+}
+
+variable "grafana_env_from_secret" {
+  description = "The name of a secret in the same kubernetes namespace which contain values to be added to the environment"
+  type        = string
+  default     = ""
+}
+
+variable "grafana_enable_service_links" {
+  description = "Inject Kubernetes services as environment variables."
+  type        = bool
+  default     = true
+}
+
+variable "grafana_extra_secret_mounts" {
+  description = "Additional grafana server secret mounts"
+  type        = list(any)
+  default     = []
+}
+
+variable "grafana_extra_volume_mounts" {
+  description = "Additional grafana server volume mounts"
+  type        = list(any)
+  default     = []
+}
+
+variable "grafana_command" {
+  description = "Define command to be executed at startup by grafana container"
+  type        = list(any)
+  default     = []
+}
+
+variable "grafana_plugins" {
+  description = "List of plugins to install"
+  default     = []
+}
+
+variable "grafana_datasources" {
+  description = "YAML string to configure grafana datasources http://docs.grafana.org/administration/provisioning/#datasources"
+  type        = string
+  default     = ""
+}
+
+variable "grafana_notifiers" {
+  description = "YAML string to configure notifiers http://docs.grafana.org/administration/provisioning/#alert-notification-channels"
+  type        = string
+  default     = ""
+}
+
+variable "grafana_dashboard_providers" {
+  description = "YAML string to configure grafana dashboard providersref: http://docs.grafana.org/administration/provisioning/#dashboards `path` must be /var/lib/grafana/dashboards/<provider_name>"
+  type        = string
+  default     = ""
+}
+
+variable "grafana_dashboards" {
+  description = "YAML string to configure grafana dashboard to import"
+  type        = string
+  default     = ""
+}
+
+variable "grafana_dashboards_config_maps" {
+  description = "Reference to external ConfigMap per provider. Use provider name as key and ConfiMap name as value. YAML string"
+  type        = string
+  default     = ""
+}
+
+### Grafana Main Config ###
+variable "grafana_main_config" {
+  description = "Main Config file in YAML"
+  type        = string
+
+  default = <<EOF
+paths:
+  data: /var/lib/grafana/data
+  logs: /var/log/grafana
+  plugins: /var/lib/grafana/plugins
+  provisioning: /etc/grafana/provisioning
+analytics:
+  check_for_updates: true
+log:
+  mode: console
+grafana_net:
+  url: https://grafana.net
+EOF
+}
+
+### Grafana DB Config ###
+variable "grafana_db_config" {
+  description = "database config for grafana"
+  type        = string
+  default     = ""
+}
+
+### Grafana Okta Config ###
+variable "grafana_okta_config" {
+  description = "okta config for grafana"
+  type        = string
+  default     = ""
+}
+
+variable "grafana_okta_enabled" {
+  description = "okta config to enable"
+  type        = bool
+  default     = false
+}
+
+variable "GRAFANA_OAUTH_CLIENT_ID" {
+  description = "GRAFANA_OAUTH_CLIENT_ID"
+  type        = string
+  default     = ""
+}
+
+variable "GRAFANA_OAUTH_CLIENT_SECRET" {
+  description = "GRAFANA_OAUTH_CLIENT_SECRET"
+  type        = string
+  default     = ""
+}
+
+### CLUSTER Config where grafana is deployed ###
+variable "cluster_name" {
+  description = "Cluster Name where Grafana is deployed"
+  type        = string
+  default     = ""
+}
+
+variable "cluster_oidc_issuer_url" {
+  description = "Cluster OIDC URL"
+  type        = string
+  default     = ""
+}
+
+variable "cluster_oidc_issuer_arn" {
+  description = "Cluster OIDC URL"
+  type        = string
+  default     = ""
+}
+
+### Grafana LDAP Config ###
+variable "grafana_ldap_existing_secret" {
+  description = "Use an existing secret for LDAP config"
+  type        = string
+  default     = ""
+}
+
+variable "grafana_ldap_config" {
+  description = "String with contents for LDAP configuration in TOML"
+  type        = string
+  default     = ""
+}
+
+### SMTP config of grafana ###
+variable "grafana_smtp_existing_secret" {
+  description = "Existing secret containing the SMTP credentials"
+  type        = string
+  default     = ""
+}
+
+variable "grafana_smtp_user_key" {
+  description = "Key in the secret containing the SMTP username"
+  type        = string
+  default     = "user"
+}
+
+variable "grafana_smtp_password_key" {
+  description = "Key in the secret containing the SMTP password"
+  type        = string
+  default     = "password"
+}
+
+### Grafana RBAC ###
+variable "grafana_psp_enable" {
+  description = "Enable PSP"
+  type        = bool
+  default     = true
+}
+
+variable "grafana_psp_use_app_armor" {
+  description = "Use AppAmor in the PSP"
+  type        = bool
+  default     = true
+}
+
+variable "grafana_pdb" {
+  description = "PodDisruptionBudget for Grafana"
+  type        = map(any)
+  default = {
+    minAvailable = 1
+  }
+}
+
+### Grafana Image Rendered ###
+variable "image_renderer_enabled" {
+  description = "Enable the image-renderer deployment and service"
+  type        = bool
+  default     = true
+}
+
+variable "image_renderer_replicas" {
+  description = "Number of replicas of image-renderer to run"
+  type        = number
+  default     = 1
+}
+
+variable "image_renderer_image_repository" {
+  description = "image-renderer Image repository"
+  type        = string
+  default     = "grafana/grafana-image-renderer"
+}
+
+variable "image_renderer_image_tag" {
+  description = "image-renderer Image tag"
+  type        = string
+  default     = "latest"
+}
+
+variable "image_renderer_env" {
+  description = "image-renderer extra environment variables"
+  type        = map(any)
+  default = {
+    HTTP_HOST = "0.0.0.0"
+  }
+}
+
+variable "image_renderer_service_account" {
+  description = "image-renderer deployment serviceAccount"
+  type        = string
+  default     = ""
+}
+
+variable "image_renderer_priority_class_name" {
+  description = "image-renderer deployment priority class"
+  type        = string
+  default     = ""
+}
+
+variable "image_renderer_security_context" {
+  description = "image-renderer deployment securityContext"
+  type        = any
+  default     = {}
+}
+
+variable "image_renderer_port" {
+  description = "image-renderer service port used by both service and deployment"
+  type        = string
+  default     = "8081"
+}
+
+variable "image_renderer_target_port" {
+  description = "image-renderer service targetPort used by both service and deployment"
+  type        = string
+  default     = "8081"
+}
+
+variable "image_renderer_resources" {
+  description = "Resources for image-renderer container"
+  type        = map(any)
+  default     = {}
+}
+
+variable "image_renderer_annotations" {
+  description = "image-renderer deployment annotations"
+  type        = map(any)
+  default     = {}
+}
+
+variable "image_renderer_pod_annotations" {
+  description = "image-renderer pod annotations"
+  type        = map(any)
+  default     = {}
 }
